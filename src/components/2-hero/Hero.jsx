e22e6f2593c5731dd17c31f11a04f5fee3e959f9
Mohamed Abdul Fatah <120588577+MOHAMMED2207@users.noreply.github.com>
2024-06-09 20:28:59 +0300

    Update Hero.jsx

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
             initial={{ transform: "scale(0)" }}
             animate={{ transform: "scale(1.1)" }}
             transition={{ damping: 6, type: "spring", stiffness: 100 }}
-            src="/img/Myphoto.png"
+            src="/img/Myphoto.JPG"
             className="avatar"
             
             alt="MyPhoto"
