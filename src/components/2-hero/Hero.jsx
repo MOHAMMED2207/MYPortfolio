7fcf1afa08ef906f8f2b27ca621e12e0d47b6849
Mohamed Abdul Fatah <120588577+MOHAMMED2207@users.noreply.github.com>
2025-10-20 19:02:32 +0300

    Enhance accessibility and structure in Hero component
    Refactor Hero component to improve accessibility and structure.

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -3,75 +3,100 @@ import "./hero.css";
 import devAnimation from "../../animation/dev.json";
 import { Link } from "react-router-dom";
 import LazyLoad from "react-lazyload";
+import "../../fonts/style.css"; // ✅ استدعاء أيقونات IcoMoon
 
 const Hero = () => {
   return (
     <section className="hero flex">
+      {/* 🔹 الجزء الأيسر */}
       <div className="left-section">
+        {/* الصورة الشخصية */}
         <div className="parent-avatar flex">
           <LazyLoad height={200} once={true} offset={100}>
             <img
               src="/img/Myphoto.png"
               className="avatar"
               alt="MyPhoto"
-              loading="lazy" // تحسين تحميل الصور
+              loading="lazy"
             />
           </LazyLoad>
-          <div className="icon-verified"></div>
+          <div className="icon-verified" title="Verified"></div>
         </div>
 
+        {/* العنوان */}
         <h1 className="title">
           Software designer, founder, and amateur astronaut.
         </h1>
 
+        {/* الوصف */}
         <p className="sub-title">
-          I'm Skilled Front-End Web Developer With Experience In JavaScript And
-          TypeScript, And Expertise In ReactJS Framework Also Build Many
-          Responsive Web Applications And User Interfaces. I'm A Quick Learner
-          And Cooperative Person. I'm Passionate About Learning New Tools And
-          Technologies. My Goal To Be A Better Developer. Let's Work Together
-          Bring Vision Into Reality.
+          I'm a skilled Front-End Web Developer with experience in JavaScript and
+          TypeScript, and expertise in the ReactJS framework. I have built many
+          responsive web applications and user interfaces. I'm a quick learner and
+          cooperative person, passionate about learning new tools and technologies.
+          My goal is to be a better developer. Let's work together to bring vision
+          into reality.
         </p>
-  {/* 🔹 الأيقونات الاجتماعية */}
+
+        {/* 🔹 الأيقونات الاجتماعية */}
         <div className="all-icons flex">
-          <Link to="https://twitter.com/" target="_blank">
-            <div className="icon icon-twitter"></div>
-          </Link>
-
-          <Link to="https://www.instagram.com/moo_mohammed_64/" target="_blank">
-            <div className="icon icon-instagram"></div>
-          </Link>
-
-          <Link to="https://github.com/MOHAMMED2207" target="_blank">
-            <div className="icon icon-github"></div>
-          </Link>
-
-          <Link
-            to="https://www.linkedin.com/in/mohammed-abd-alfatah-58959a256/"
+          <a
+            href="https://twitter.com/"
             target="_blank"
-          >
-            <div className="icon icon-linkedin"></div>
-          </Link>
+            rel="noopener noreferrer"
+            className="icon icon-twitter"
+            aria-label="Twitter"
+          ></a>
+
+          <a
+            href="https://www.instagram.com/moo_mohammed_64/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon icon-instagram"
+            aria-label="Instagram"
+          ></a>
+
+          <a
+            href="https://github.com/MOHAMMED2207"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon icon-github"
+            aria-label="GitHub"
+          ></a>
+
+          <a
+            href="https://www.linkedin.com/in/mohammed-abd-alfatah-58959a256/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon icon-linkedin"
+            aria-label="LinkedIn"
+          ></a>
         </div>
 
-        {/* 🔸 أيقونات المهارات البرمجية */}
+        {/* 🔸 أيقونات المهارات */}
         <div className="skills-icons flex">
-          <img src="/icons/js.svg" alt="JavaScript" title="JavaScript" />
-          <img src="/icons/ts.svg" alt="TypeScript" title="TypeScript" />
-          <img src="/icons/react.svg" alt="React.js" title="React.js" />
-          <img src="/icons/nextjs.svg" alt="Next.js" title="Next.js" />
-          <img src="/icons/nodejs.svg" alt="Node.js" title="Node.js" />
-          <img src="/icons/mongodb.svg" alt="MongoDB" title="MongoDB" />
-          <img src="/icons/tailwind.svg" alt="Tailwind CSS" title="Tailwind CSS" />
-          <img src="/icons/git.svg" alt="Git" title="Git" />
-          <img src="/icons/vscode.svg" alt="VS Code" title="VS Code" />
+          {[
+            { src: "/icons/js.svg", alt: "JavaScript" },
+            { src: "/icons/ts.svg", alt: "TypeScript" },
+            { src: "/icons/react.svg", alt: "React.js" },
+            { src: "/icons/nextjs.svg", alt: "Next.js" },
+            { src: "/icons/nodejs.svg", alt: "Node.js" },
+            { src: "/icons/mongodb.svg", alt: "MongoDB" },
+            { src: "/icons/tailwind.svg", alt: "Tailwind CSS" },
+            { src: "/icons/git.svg", alt: "Git" },
+            { src: "/icons/vscode.svg", alt: "VS Code" },
+          ].map(({ src, alt }) => (
+            <img key={alt} src={src} alt={alt} title={alt} loading="lazy" />
+          ))}
         </div>
       </div>
+
+      {/* 🔹 الجزء الأيمن */}
       <div className="right-section animation">
         <Lottie
           animationData={devAnimation}
           loop={true}
-          style={{ width: "100%", height: "auto" }} // ضبط حجم الرسوم المتحركة
+          style={{ width: "100%", height: "auto" }}
         />
       </div>
     </section>
@@ -79,3 +104,4 @@ const Hero = () => {
 };
 
 export default Hero;
+
